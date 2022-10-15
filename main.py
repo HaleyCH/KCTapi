22c636fe4e3720578e01d2cbba7ee2eefa347165
HaleyC <1617335718@qq.com>
2022-10-15 19:31:38 +0800

    Add argparse to main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 import datetime
+import argparse
 
 app = FastAPI()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="KCT-api")
+    parser.add_argument("--port", type=int, default=4999, help="--port int:port")
+    args = parser.parse_args()
+    return args
 
 
 @app.get("/")
@@ -13,4 +21,6 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app)
+
+    args = parse_args()
+    uvicorn.run(app, port=args.port)
